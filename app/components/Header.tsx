2e616f1fb186e0c813172686c0437b5db36dcc79
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-06 11:35:15 +0900

    feat: use HeaderDrawer component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,20 +2,27 @@ import {
   Button,
   ButtonGroup,
   Flex,
+  Hide,
   IconButton,
+  Show,
   Spacer,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { useRef } from "react";
 import { MdMenu } from "react-icons/md";
+import { HeaderDrawer } from "./HeaderDrawer";
+import { RegisterAndLoginButton } from "./RegisterAndLoginButton";
 
 export function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef(null);
   return (
     <Flex
       bg="teal.100"
       minW="max-content"
       minH="16"
       alignItems="center"
-      gap="2"
       px="4"
       py="2"
     >
@@ -23,15 +30,22 @@ export function Header() {
         <Text fontSize="xl">Syllanavi</Text>
       </Button>
       <Spacer />
-      <ButtonGroup display={["none", "contents"]}>
-        <Button>新規登録</Button>
-        <Button>ログイン</Button>
-      </ButtonGroup>
-      <IconButton
-        aria-label="Menu button"
-        display={["contents", "none"]}
-        icon={<MdMenu />}
-      />
+      <Show above="sm">
+        <RegisterAndLoginButton />
+      </Show>
+      <Hide above="sm">
+        <IconButton
+          aria-label="Menu button"
+          icon={<MdMenu />}
+          ref={btnRef}
+          onClick={onOpen}
+        />
+        <HeaderDrawer
+          isOpen={isOpen}
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        />
+      </Hide>
     </Flex>
   );
 }
