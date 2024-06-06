5c5df63fc70c9f251d60d070f7795400316f254f
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-07 02:30:43 +0900

    feat(db): implement create and util funcs for users model

---
+++ app/db/repository/users.ts
@@ -0,0 +1,28 @@
+import { eq, type InferInsertModel } from "drizzle-orm";
+import type { DrizzleD1Database } from "drizzle-orm/d1";
+import type * as Schema from "~/db/schema";
+import { users } from "~/db/schema";
+
+export const Users = (db: DrizzleD1Database<typeof Schema>) => {
+  return {
+    async create(data: InferInsertModel<typeof users>) {
+      return (
+        await db.insert(users).values(data).returning({
+          id: users.id,
+          name: users.name,
+        })
+      )[0];
+    },
+    async checkNameDuplicate(name: string) {
+      const user = await db.query.users.findFirst({
+        where: eq(users.name, name),
+      });
+
+      if (user) {
+        return true;
+      }
+
+      return false;
+    },
+  };
+};
