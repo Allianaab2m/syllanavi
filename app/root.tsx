2914f8b55cb830580fbfb934c4a4f3a0f010d023
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-06 13:43:58 +0900

    chore: init mantine

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 import {
   Links,
   Meta,
@@ -14,9 +16,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <ColorSchemeScript />
       </head>
       <body>
-        {children}
+        <MantineProvider>{children}</MantineProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
