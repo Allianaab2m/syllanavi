3519f83517183cf0e4bae687e7aa776f74152a5d
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-07 02:33:08 +0900

    feat(components): add ModalButton component

---
+++ app/components/ModalButton.tsx
@@ -0,0 +1,30 @@
+import { Button, Modal, type ButtonProps } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import type { ReactNode } from "react";
+
+type Props = {
+  title: string;
+  children: ReactNode;
+  modalContent: ReactNode;
+  buttonProps?: ButtonProps;
+};
+
+export function ModalButton({
+  title,
+  children,
+  modalContent,
+  buttonProps,
+}: Props) {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  return (
+    <>
+      <Modal title={title} opened={opened} onClose={close} centered>
+        {modalContent}
+      </Modal>
+      <Button {...buttonProps} onClick={open}>
+        {children}
+      </Button>
+    </>
+  );
+}
