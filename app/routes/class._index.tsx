47dd7b9c5dc96789aa5596ad01c5bf9fcef9327e
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-09 07:57:50 +0900

    feat: define tentative routes

---
+++ app/routes/class._index.tsx
@@ -0,0 +1,5 @@
+import { Text } from "@mantine/core";
+
+export default function ClassRoute() {
+  return <Text>授業一覧</Text>;
+}
---
+++ app/routes/class.create.tsx
@@ -0,0 +1,5 @@
+import { Text } from "@mantine/core";
+
+export default function ClassCreate() {
+  return <Text>新規授業作成</Text>;
+}
