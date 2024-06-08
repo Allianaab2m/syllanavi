80a36807e35ce60b892d4ed88f28d73476f51a5c
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-08 13:31:31 +0900

    chore: use Result, Nullable type

--- app/db/repository/users.ts
+++ app/db/repository/users.ts
@@ -1,6 +1,8 @@
 import type { InferInsertModel } from "drizzle-orm";
 import { eq } from "drizzle-orm";
 import type { DrizzleD1Database } from "drizzle-orm/d1";
+import type { Nullable } from "option-t/nullable";
+import type { Result } from "option-t/plain_result";
 import { createOk } from "option-t/plain_result";
 import type * as Schema from "~/db/schema";
 import { users } from "~/db/schema";
@@ -8,7 +10,9 @@ import { wrapErr } from "~/lib";
 
 export const Users = (db: DrizzleD1Database<typeof Schema>) => {
   return {
-    async create(data: InferInsertModel<typeof users>) {
+    async create(
+      data: InferInsertModel<typeof users>,
+    ): Promise<Result<{ id: string; name: string }, Error>> {
       try {
         const user = (
           await db.insert(users).values(data).returning({
@@ -21,16 +25,25 @@ export const Users = (db: DrizzleD1Database<typeof Schema>) => {
         return wrapErr(e);
       }
     },
-    async checkNameDuplicate(name: string) {
-      const user = await db.query.users.findFirst({
-        where: eq(users.name, name),
-      });
-
-      if (user) {
-        return true;
+    async findByName(
+      name: string,
+    ): Promise<
+      Nullable<{ id: string; name: string; isAdmin: boolean | null }>
+    > {
+      try {
+        const user = await db.query.users.findFirst({
+          where: eq(users.name, name),
+          columns: {
+            password: false,
+          },
+        });
+        if (!user) {
+          return null;
+        }
+        return user;
+      } catch (e) {
+        return null;
       }
-
-      return false;
     },
   };
 };
