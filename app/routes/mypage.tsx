361c711693f0408bf5da509dabdafb520acf0e03
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-09 06:46:58 +0900

    feat(mypage): define mypage route

---
+++ app/routes/mypage.tsx
@@ -0,0 +1,36 @@
+import { Box, Text } from "@mantine/core";
+import { json, redirect, type LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/db";
+import { Users } from "~/db/repository/users";
+import { getUserSession } from "~/sessions";
+
+export async function loader({ context, request }: LoaderFunctionArgs) {
+  const session = await getUserSession(context)(request);
+  if (session.userId) {
+    const user = await Users(db(context)).findById(session.userId);
+    if (user) {
+      return json({
+        success: true,
+        error: null,
+        user,
+      });
+    }
+  }
+  return redirect("/login");
+}
+
+export default function MyPage() {
+  const { user } = useLoaderData<typeof loader>();
+  return user ? (
+    <Box mx="xl">
+      <Text fw="bold" size="xl">
+        マイページ
+      </Text>
+      <Text>ID: {user.id}</Text>
+      <Text>Name: {user.name}</Text>
+    </Box>
+  ) : (
+    <></>
+  );
+}
