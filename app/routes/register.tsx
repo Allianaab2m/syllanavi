1364b8bb368a2c826379dcf30590126f70119838
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-08 14:49:24 +0900

    chore(routes/register): update

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,13 +1,24 @@
 import { getFormProps, getInputProps, useForm } from "@conform-to/react";
 import { parseWithZod } from "@conform-to/zod";
-import { Button, Stack, TextInput } from "@mantine/core";
+import { Box, Button, Stack, Text, TextInput } from "@mantine/core";
 import type { ActionFunctionArgs } from "@remix-run/cloudflare";
-import { json } from "@remix-run/cloudflare";
-import { Form, useActionData } from "@remix-run/react";
-import { useEffect } from "react";
+import { json, redirect } from "@remix-run/cloudflare";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
+import { isErr } from "option-t/plain_result";
 import { z } from "zod";
 import { db } from "~/db";
 import { Users } from "~/db/repository/users";
+
+export const RegisterSchema = z.object({
+  name: z
+    .string({ required_error: "この項目は必須です" })
+    .min(4, { message: "4文字以上で入力してください" })
+    .max(16, { message: "16文字以下で入力してください" })
+    .regex(/[0-9a-zA-Z]+/, { message: "英数字のみで入力してください" }),
+  password: z
+    .string({ required_error: "この項目は必須です" })
+    .min(8, { message: "8文字以上で入力してください" }),
+});
 
 export async function action({ request, context }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -22,9 +33,7 @@ export async function action({ request, context }: ActionFunctionArgs) {
     });
   }
 
-  const existUser = await users.findByName(submission.value.name);
-
-  if (existUser) {
+  if (await users.findByName(submission.value.name)) {
     return json({
       success: false,
       message: "Name duplicate",
@@ -36,8 +45,7 @@ export async function action({ request, context }: ActionFunctionArgs) {
     });
   }
 
-  const userCreateRes = await users.create({ name: submission.value.name });
-  if (userCreateRes.err) {
+  if (isErr(await users.create({ name: submission.value.name }))) {
     return json({
       success: false,
       message: "DB Error",
@@ -49,60 +57,50 @@ export async function action({ request, context }: ActionFunctionArgs) {
     });
   }
 
-  return json({
-    success: true,
-    message: "success",
-    submission: submission.reply(),
-  });
+  return redirect("/");
 }
-
-export const RegisterSchema = z.object({
-  name: z
-    .string()
-    .min(4, { message: "4文字以上で入力してください" })
-    .max(16, { message: "16文字以下で入力してください" })
-    .regex(/[0-9a-zA-Z]+/, { message: "英数字のみで入力してください" }),
-  password: z
-    .string()
-    .min(8, { message: "8文字以上入力してください" })
-    .regex(/[0-9]+/, { message: "数字を1文字以上使用してください" })
-    .regex(/[a-z]+/, { message: "英小文字を1文字以上使用してください" })
-    .regex(/[A-Z]+/, { message: "英大文字を1文字以上使用してください" }),
-});
 
 export default function Register() {
   const data = useActionData<typeof action>();
+  const { state } = useNavigation();
+
   const [form, { name, password }] = useForm({
     lastResult: data?.submission,
     onValidate({ formData }) {
       return parseWithZod(formData, { schema: RegisterSchema });
     },
     shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
   });
 
-  useEffect(() => {
-    if (!data) return;
-    console.log(data);
-  }, [data]);
-
   return (
-    <Form navigate={false} method="POST" {...getFormProps(form)}>
-      <Stack>
-        <TextInput
-          withAsterisk
-          label="ユーザー名"
-          placeholder="英数字のみ 4~16文字"
-          error={name.errors?.toString()}
-          {...getInputProps(name, { type: "text" })}
-        />
-        <TextInput
-          withAsterisk
-          label="パスワード"
-          placeholder="英数字・記号 8文字以上"
-          {...getInputProps(password, { type: "password" })}
-        />
-        <Button type="submit">登録</Button>
-      </Stack>
-    </Form>
+    <Box mt="lg">
+      <Text size="xl" fw="bold">
+        新規登録
+      </Text>
+      <Form method="POST" {...getFormProps(form)}>
+        <Stack>
+          <TextInput
+            withAsterisk
+            disabled={state === "submitting"}
+            label="ユーザー名"
+            description="英数字のみ。4文字以上16文字以下で入力してください。"
+            error={name.errors}
+            {...getInputProps(name, { type: "text" })}
+          />
+          <TextInput
+            withAsterisk
+            disabled={state === "submitting"}
+            label="パスワード"
+            description="英数字・記号が使えます。8文字以上で入力してください。"
+            error={password.errors}
+            {...getInputProps(password, { type: "password" })}
+          />
+          <Button loading={state === "submitting"} color="teal.6" type="submit">
+            登録
+          </Button>
+        </Stack>
+      </Form>
+    </Box>
   );
 }
