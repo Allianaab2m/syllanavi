699ec38b39627f9a82e6cc6ef616a16e410acf0a
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-06-20 23:30:26 +0900

    chore: update

--- app/lib.ts
+++ app/lib.ts
@@ -10,18 +10,24 @@ export const wrapErr = (e: unknown) => {
 export const Day = ["月", "火", "水", "木", "金", "土"] as const;
 export const Term = ["前期", "後期"] as const;
 
-export const serializeDay = (day: (typeof Day)[number]) => {
+export const serializeDay = (day: string) => {
   return Day.findIndex((v) => v === day) + 1;
 };
 
 export const deserializeDay = (dayNumber: number) => {
-  return Day[dayNumber - 1];
+  if (Day[dayNumber - 1]) {
+    return Day[dayNumber - 1];
+  }
+  return "未設定";
 };
 
-export const serializeTerm = (term: (typeof Term)[number]) => {
+export const serializeTerm = (term: string) => {
   return Term.findIndex((v) => v === term) + 1;
 };
 
 export const deserializeTerm = (termNumber: number) => {
-  return Term[termNumber - 1];
+  if (Term[termNumber - 1]) {
+    return Term[termNumber - 1];
+  }
+  return "未設定";
 };
--- app/routes/class._index.tsx
+++ app/routes/class._index.tsx
@@ -1,13 +1,24 @@
-import { Table, TextInput, Title, keys, rem } from "@mantine/core";
+import {
+  Group,
+  NumberInput,
+  Select,
+  Table,
+  TextInput,
+  Title,
+  rem,
+} from "@mantine/core";
 import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 import { isErr } from "option-t/plain_result";
 import type React from "react";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Search } from "tabler-icons-react";
-import { ClassTable } from "~/components";
+import { ClassTable, ClassTableHeader, filterData } from "~/components";
 import { db } from "~/db";
-import { type Class, Classes } from "~/db/repository/classes";
+import { Classes } from "~/db/repository/classes";
+import { findDepartmentByID, type Category } from "~/db/repository/departments";
+import departments from "~/departments.json";
+import { Day, serializeDay, serializeTerm } from "~/lib";
 import { getUserSession } from "~/sessions";
 
 export async function loader({ context, request }: LoaderFunctionArgs) {
@@ -29,43 +40,80 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
   };
 }
 
-function ClassTableHeader() {
-  return (
-    <Table.Thead>
-      <Table.Tr>
-        <Table.Th>ID</Table.Th>
-        <Table.Th>授業名</Table.Th>
-        <Table.Th>学科</Table.Th>
-        <Table.Th>区分</Table.Th>
-        <Table.Th>曜日・時限</Table.Th>
-        <Table.Th>開講年次</Table.Th>
-        <Table.Th>学期</Table.Th>
-        <Table.Th>単位数</Table.Th>
-      </Table.Tr>
-    </Table.Thead>
-  );
-}
-
-function filterData(data: Class[] | null, search: string) {
-  if (!data) return [];
-  const query = search.toLowerCase().trim();
-  const filtered = data?.filter((item) =>
-    item.name.toLowerCase().includes(query),
-  );
-
-  if (!filtered) return [];
-  return filtered;
-}
+type Filter = {
+  search: string | null;
+  department: string | null;
+  category: string | null;
+  day: string | null;
+  academicYear: string | null;
+  term: string | null;
+  time: string | null;
+};
 
 export default function ClassRoute() {
   const { data, error, userId } = useLoaderData<typeof loader>();
   const classData = useMemo(() => data, [data]);
+  const [categories, setCategories] = useState<Category[] | undefined>(
+    undefined,
+  );
+  const [filter, setFilter] = useState<Filter>({
+    search: "",
+    department: null,
+    category: null,
+    day: null,
+    academicYear: null,
+    term: null,
+    time: null,
+  });
   const [filteredData, setFilteredData] = useState(classData);
 
+  useEffect(() => {
+    setFilteredData(
+      filterData(classData, {
+        search: filter.search,
+        department: Number(filter.department),
+        category: filter.category ? Number(filter.category) : -1,
+        day: serializeDay(filter.day ?? ""),
+        academicYear: Number(filter.academicYear),
+        term: serializeTerm(filter.term ?? ""),
+        time: Number(filter.time),
+      }),
+    );
+  }, [classData, filter]);
+
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.currentTarget;
-    console.log(value);
-    setFilteredData(filterData(classData, value));
+    setFilter({ ...filter, search: event.currentTarget.value });
+  };
+
+  const handleDepartmentChange = (value: string | null) => {
+    setFilter({
+      ...filter,
+      department: value,
+    });
+    setCategories(findDepartmentByID(Number(value))?.categories);
+  };
+
+  const handleCategoryChange = (value: string | null) => {
+    setFilter({
+      ...filter,
+      category: value,
+    });
+  };
+
+  const handleDayChange = (value: string | null) => {
+    setFilter({ ...filter, day: value });
+  };
+
+  const handleAcademicYearChange = (value: string | null) => {
+    setFilter({ ...filter, academicYear: value });
+  };
+
+  const handleTimeChange = (value: string | number) => {
+    setFilter({ ...filter, time: value.toString() });
+  };
+
+  const handleTermChange = (value: string | null) => {
+    setFilter({ ...filter, term: value });
   };
 
   return (
@@ -77,8 +125,49 @@ export default function ClassRoute() {
         placeholder="授業名で検索..."
         onChange={handleSearchChange}
       />
+      <Group>
+        <Select
+          placeholder="学科"
+          data={departments.map((d) => ({
+            value: d.id.toString(),
+            label: d.name,
+          }))}
+          onChange={handleDepartmentChange}
+        />
+        <Select
+          withAsterisk
+          placeholder="区分"
+          data={categories?.map((c) => ({
+            value: c.id.toString(),
+            label: c.name,
+          }))}
+          onChange={handleCategoryChange}
+        />
+        <Select placeholder="曜日" data={Day} onChange={handleDayChange} />
+        <NumberInput
+          placeholder="時限"
+          allowNegative={false}
+          min={1}
+          onChange={(e) => {
+            handleTimeChange(e);
+          }}
+        />
+        <Select
+          placeholder="開講年次"
+          data={["1", "2", "3", "4"].map((v) => ({
+            value: v,
+            label: `${v}年`,
+          }))}
+          onChange={handleAcademicYearChange}
+        />
+        <Select
+          placeholder="学期"
+          data={["前期", "後期"]}
+          onChange={handleTermChange}
+        />
+      </Group>
       {filteredData ? (
-        <Table stickyHeader highlightOnHover>
+        <Table stickyHeader highlightOnHover layout="fixed">
           <ClassTableHeader />
           <Table.Tbody>
             {filteredData.map((d) => (
